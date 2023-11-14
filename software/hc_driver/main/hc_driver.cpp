058a9d851fb41a4b2b540ce660a71c652ca7687a
Ramon Borba <ramonborba97@gmail.com>
2023-11-14 11:52:10 -0300

    software: hc_driver: main: Added file description

--- software/hc_driver/main/hc_driver.cpp
+++ software/hc_driver/main/hc_driver.cpp
@@ -1,4 +1,14 @@
-#include <stdio.h>
+/**
+ * \file hc_driver.cpp
+ * 
+ * \brief Application entry point
+ * 
+ * \author Ramon de Araujo Borba <ramonborba97@gmail.com>
+ * 
+ * \version 0.1
+ * 
+ * \date 14-11-2023
+ */
 
 extern "C" void app_main(void)
 {
