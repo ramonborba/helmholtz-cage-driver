b59bda6a36c4a0c4164b23d6ad83aafbc377525b
Ramon Borba <ramonborba97@gmail.com>
2023-11-14 11:52:58 -0300

    software: hc_driver: tasks: Created tasks_api component

---
+++ software/hc_driver/tasks/tasks_api/include/tasks_api.hpp
@@ -0,0 +1,17 @@
+/**
+ * \file tasks_api.hpp
+ * 
+ * \brief API for handling application tasks
+ * 
+ * \author Ramon de Araujo Borba <ramonborba97@gmail.com>
+ * 
+ * \version 0.1
+ * 
+ * \date 14-11-2023
+ */
+
+/**
+ * \brief Creates all applicaion tasks
+ * 
+ */
+void create_tasks(void);
---
+++ software/hc_driver/tasks/tasks_api/tasks_api.cpp
@@ -0,0 +1,17 @@
+/**
+ * \file tasks_api.cpp
+ * 
+ * \brief Tasks API implementation
+ * 
+ * \author Ramon de Araujo Borba <ramonborba97@gmail.com>
+ * 
+ * \version 0.1
+ * 
+ * \date 14-11-2023
+ */
+#include "tasks_api.hpp"
+
+void create_tasks(void)
+{
+
+}
